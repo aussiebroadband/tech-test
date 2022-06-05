f0f2e558dba07527956667009b006225ab3e49f1
shoaibch40 <shoaibch40@gmail.com>
2022-06-06 02:40:29 +0500

    Feat: add relations between models

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -6,6 +6,7 @@ use App\Enums\ApplicationQueues;
 use App\Events\ApplicationCreated;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Application extends Model
 {
@@ -18,4 +19,14 @@ class Application extends Model
     protected $dispatchesEvents = [
         'created' => ApplicationCreated::class,
     ];
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class, 'customer_id');
+    }
+
+    public function plan(): BelongsTo
+    {
+        return $this->belongsTo(Plan::class, 'plan_id');
+    }
 }
--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Plan extends Model
 {
     use HasFactory;
+
+    public function application(): HasMany
+    {
+        return $this->hasMany(Application::class);
+    }
+
+
 }
