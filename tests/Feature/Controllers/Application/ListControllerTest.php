c37dd68077b38cbf5676f67b270da45bb18accf7
shoaibch40 <shoaibch40@gmail.com>
2022-06-06 06:38:07 +0500

    Feat: Test Cases for List Application API

--- tests/Feature/Controllers/Application/ListControllerTest.php
+++ tests/Feature/Controllers/Application/ListControllerTest.php
@@ -7,21 +7,43 @@ use Tests\TestCase;
 
 class ListControllerTest extends TestCase
 {
-    public function test_it_can_throw_unauthorize_if_email_is_wrong()
+    public function test_it_can_insert_applications()
     {
-//        Application::factory(10)->create();
+        Application::factory(10)->create();
 
-        $createUserToken = $this->call('POST', $this->baseUri . '/token/create', ['email' => '' /* email here */]);
+        $this->assertEquals(10, Application::all()->count());
+    }
 
-        $user = $createUserToken->getContent();
+    public function test_it_can_throw_unauthorize_if_token_is_not_passed()
+    {
+        $response = $this->call('GET', $this->baseUri . '/application');
 
-        $response = $this->call('POST', $this->baseUri . '/application',
-            [/* params */],
-            [/* cookies */],
-            [/* files */],
-            ['HTTP_Authorization' => 'Bearer ' . null]);
+        $this->assertEquals(403, $response->getStatusCode());
+    }
+
+    public function test_it_can_list_applications_with_order_id()
+    {
+        $this->withoutMiddleware();
+
+        Application::factory(10)->create(['order_id' => 1]);
+
+        $response = $this->call('GET', $this->baseUri . '/application');
+
+        $this->assertCount(10, $response->json()["data"]);
+    }
+
+    public function test_it_can_paginate()
+    {
+        $this->withoutMiddleware();
+
+        Application::factory(10)->create(['order_id' => 1]);
+
+        $response = $this->call('GET', $this->baseUri . '/application');
 
 
-        $this->assertEquals(404, $createUserToken->getStatusCode());
+        $this->assertEquals(true, array_key_exists('data', $response->json())
+            && array_key_exists('meta', $response->json())
+            && array_key_exists('links', $response->json())
+        );
     }
 }
