ee098e0e7d8d262eff6229797b3171126f8f4f5f
Carol Saldanha <carol@technium.com.au>
2023-10-18 20:15:39 +1100

    Create the rest of DB seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(20)->create();
+        \App\Models\Customer::factory(20)->create();
+        \App\Models\Application::factory(20)->create();
     }
 }
