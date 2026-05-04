24b0e97571158e020a2ee83b7429ef380e3777b6
rick.measham <rick.measham@team.aussiebroadband.com.au>
2026-05-04 03:41:53 +0000

    Revert "chore: add Eloquent relationships and $fillable to models"
    This reverts commit 01e61a723f9ba58c67180ee43f15dcef68917688.

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,25 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Customer extends Model
 {
     use HasFactory;
-    use SoftDeletes;
-
-    protected $fillable = [
-        'first_name',
-        'last_name',
-        'email',
-        'mobile',
-    ];
-
-    public function applications(): HasMany
-    {
-        return $this->hasMany(Application::class);
-    }
 }
